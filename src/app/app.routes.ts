220904097c06d33f9e6c192f8ce8fd60443992ac
iagopedro <iagopedroltd19@gmail.com>
2025-09-24 12:28:29 -0300

    primeiro commit :tada:

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: Home },
+    { path: "login", component: Login },
+    { path: "", redirectTo: "/home", pathMatch: 'full' }, 
+];
